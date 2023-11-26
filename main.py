a01567f8bdc22f9186528b14219f37243bf2ba00
vicleca <victorleocanto@gmail.com>
2023-11-26 19:40:10 +0100

    Add database

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from db import models
+from db.database import engine
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return "Hello world!"
+
+models.Base.metadata.create_all(engine)
