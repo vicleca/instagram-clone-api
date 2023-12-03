4767f738026b4745fdfcb576884c27f8702be408
vicleca <victorleocanto@gmail.com>
2023-12-03 17:56:33 +0100

    Add post creation

--- db/models.py
+++ db/models.py
@@ -1,5 +1,7 @@
+from sqlalchemy.sql.schema import ForeignKey
 from .database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
 
 class DbUser(Base):
   __tablename__ = 'users'
@@ -7,3 +9,14 @@ class DbUser(Base):
   username = Column(String)
   email = Column(String)
   password = Column(String)
+  items = relationship('DbPost', back_populates='user')
+
+class DbPost(Base):
+  __tablename__ = 'posts'
+  id = Column(Integer, primary_key=True, index=True)
+  image_url = Column(String)
+  image_url_type = Column(String)
+  caption = Column(String)
+  timestamp = Column(DateTime)
+  user_id = Column(Integer, ForeignKey('users.id'))
+  user = relationship('DbUser', back_populates='items')
--- routers/schemas.py
+++ routers/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class UserBase(BaseModel):
     username: str
@@ -8,5 +9,27 @@ class UserBase(BaseModel):
 class UserDisplay(BaseModel):
     username: str
     email: str
+    class Config():
+        from_attributes = True
+
+class PostBase(BaseModel):
+    image_url: str
+    image_url_type: str
+    caption: str
+    creator_id: int
+
+# For PostDisplay
+class User(BaseModel):
+    username: str
+    class Config():
+        from_attributes = True
+
+class PostDisplay(BaseModel):
+    id: int
+    image_url: str
+    image_url_type: str
+    caption: str
+    timestamp: datetime
+    user: User
     class Config():
         from_attributes = True
