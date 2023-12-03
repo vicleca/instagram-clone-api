0726c06187ed57140830eed7db86c46f20bc80a2
vicleca <victorleocanto@gmail.com>
2023-12-03 19:33:22 +0100

    Add configuration to avoid CORS errors

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from db.database import engine
 from routers import user, post, comment
 from fastapi.staticfiles import StaticFiles
 from auth import  authentication
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(user.router)
@@ -14,6 +15,20 @@ app.include_router(authentication.router)
 @app.get("/")
 def root():
     return "Hello world!"
+
+origins = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:3002'
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=['*'],
+  allow_headers=['*']
+)
 
 models.Base.metadata.create_all(engine)
 
